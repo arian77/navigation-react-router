0ce853da91f60e737aee53de55b4895e3063e91b
Arian <arianmarkus77@yahoo.com>
2017-11-06 10:02:32 +0700

    Create three links navigation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
+const Links = () => (
+  <nav>
+    <Link to="/">Home </Link>
+    <Link to={{ pathname: "/portofolio" }}>Portofolio</Link>
+    <Link replace to="/contact">
+      Contact
+    </Link>
+  </nav>
+);
+
 const App = () => (
   <Router>
     <div>
+      <Links />
       <Route exact path="/" render={() => <h1>Home </h1>} />
       <Route path="/portofolio" render={() => <h1>Portofolio </h1>} />
       <Route path="/contact" render={() => <h1> Contact </h1>} />
